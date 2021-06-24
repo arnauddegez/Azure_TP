af4e9c2370d1e3232f306c5952d935f1247ed0ce
arnauddegez <adegez@gmail.com>
2021-06-24 11:50:20 +0200

    add tp2 file

---
+++ tp02_deploy_template/script_deploy.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+
+#Ce scrypt permet de déployer l'ensemble des ressources 
+#necessaire au bon fonctionnement d'une appli flask
+
+#Variables:
+group_name="tp2-arnaud-rg"
+location="eastus"
+group_test=$(az group exists --name $group_name)
+
+
+if $group_test == true; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [ INFO  ] : Le groupe existe déjà ..."
+
+else
+echo "$(date +'%Y-%m-%d %H:%M:%S') [ INFO  ] : Création du groupe de ressource ..."
+az group create \
+--name $group_name \
+--location $location
+
+#Deploiement de la vm + network (vnet,subnet,nat gateway, etc...)
+az deployment group create \
+--resource-group $group_name \
+--template-file  template_resource.json
+
